1da1514b0b2a2c36cf65299f95d246274822e2c9
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-08-03 17:38:41 +0200

    Logs

--- src/NPost.Services.Routing/Queries/Handlers/GetRouteHandler.cs
+++ src/NPost.Services.Routing/Queries/Handlers/GetRouteHandler.cs
@@ -27,11 +27,13 @@ namespace NPost.Services.Routing.Queries.Handlers
                 throw new Exception("Route cannot be calculated without the addresses.");
             }
 
-            _logger.LogInformation("Calculating the optimal route...");
+            var addressesInfo = string.Join(", ", query.Addresses);
+            _logger.LogInformation($"Calculating the optimal route for addresses: {addressesInfo}");
             // Let's assume it's a time consuming calculation.
             await Task.Delay(3000);
             var route = _routingService.Calculate(query.Addresses);
-            _logger.LogInformation($"The optimal route was calculated, total distance: {route.TotalDistance} km.");
+            _logger.LogInformation($"The optimal route for addresses: {addressesInfo} was calculated," +
+                                   $"total distance: {route.TotalDistance} km.");
 
             return route;
         }
